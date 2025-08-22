be2c6d59a588be9a26b2dd707c831b3e374924f0
Yusuf Bey <yusuf@Yusuf-MacBook-Air.local>
2025-08-22 23:27:12 +0300

    ilk commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="form-row">
+      <div class="form-wrapper">
+        <FormImage />
+        <MyForm />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormImage from "@/components/FormImage.vue";
+import MyForm from "@/components/MyForm.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    FormImage,
+    MyForm,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="form-content">
+    <h2>İLETİŞİM</h2>
+    <form @submit="formSubmit" method="post">
+      <div class="group top">
+        <input
+          type="text"
+          name="name"
+          v-model="formData.name"
+          placeholder="Ad"
+        />
+        <input
+          type="text"
+          name="lastName"
+          v-model="formData.lastName"
+          placeholder="Soyad"
+        />
+      </div>
+      <div class="group bottom">
+        <input
+          type="email"
+          v-model="formData.email"
+          name="email"
+          placeholder="Email"
+        />
+        <textarea
+          name="message"
+          v-model="formData.message"
+          placeholder="Mesajınız..."
+        ></textarea>
+      </div>
+      <button type="submit" name="send">Gönder</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      formData: {
+        name: "",
+        lastName: "",
+        email: "",
+        message: "",
+      },
+    };
+  },
+  methods: {
+    formSubmit(e) {
+      e.preventDefault();
+      const url = "http://localhost:8000/sendMail.php";
+
+      axios({
+        method: "POST",
+        url: url,
+        data: {
+          name: this.formData.name,
+          lastName: this.formData.lastName,
+          email: this.formData.namemaile,
+          message: this.formData.namemaile,
+        },
+      })
+        .then((success) => console.log(success))
+        .catch((error) => console.error(error));
+    },
+  },
+};
+</script>
